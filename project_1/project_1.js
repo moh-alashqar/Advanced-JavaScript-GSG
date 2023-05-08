88bff2d7a4182f978aecd5ed31c006a355103e41
Alashqar <61499469+moh-alashqar@users.noreply.github.com>
2023-05-08 16:01:16 +0300

    Update project_1.js

--- project_1/project_1.js
+++ project_1/project_1.js
@@ -1,33 +1,36 @@
 class Hotel{
-    constructor(address, numberOfRooms, minFloor, maxFloor, room){
+    #minFloor;
+    #maxFloor;
+    constructor(address, numberOfRooms, minFloor, maxFloor, rooms){
         this.address = address;
         this.numberOfRooms = numberOfRooms;
         this.#minFloor = minFloor;
         this.#maxFloor = maxFloor;
-        this.rooms.push(room);
+        this.rooms = rooms;
     }
-    function printAdvertisment(){
-        console.log("Out hotel is on " + this.address + ", and has ", this.numberOfRooms+ " rooms, and " + this.#maxFloor- #minFloor + " floors.");
+    printAdvertisment(){
+        console.log("# Advertisment #\n our hotel is on " + this.address + ", and has "+ this.numberOfRooms+ " rooms.");
     }
-    function listBokkedRooms(){
-        return this.rooms.filter((r) => r.isBooked);
+    listBookedRooms(){
+        return this.rooms.filter((r) => r.isBooked());
     }
 }
 
 class Room{
+    #isBooked;
     constructor(floorNum, roomNum, price, isBooked){
         this.floorNum = floorNum;
         this.roomNum = roomNum;
         this.price = price;
         this.#isBooked = isBooked;
     }
-    function printRoom(){
-        console.log("Room no. " + this.roomNum + " in the " + this.floor + " floor ,it costs " + this.price + " USD ,and is " + this.isBooked ? "": "not" + "Booked!");
+    printRoom(){
+        console.log("# Room Info #\n room no. " + this.roomNum + " in the " + this.floor + " floor ,it costs " + this.price + " USD ,and is " + this.#isBooked ? "": "not" + "Booked!");
     }
-    function book(){
+    book(){
         this.#isBooked = true;
     }
-    function isBooked(){
+    isBooked(){
         return this.#isBooked;
     }
 }
@@ -46,3 +49,17 @@ class RoomWithView extends Room{
         this.numberOfBeds = numberOfBeds;
     }
 }
+
+var r1 = new SleepingRoom(1, 5, 20, true, 2);
+var r2 = new RoomWithView(3, 32, 50, false, 'sea', 1);
+var h1 = new Hotel('Ramallah', 50, 1, 5, [r1, r2]);
+console.log("####################")
+// console.log(h1.rooms)
+console.log("# Report #\n address " + h1.address + ", number of rooms "+ h1.numberOfRooms + ", and has", h1.rooms);
+console.log("####################")
+h1.printAdvertisment();
+console.log("####################")
+console.log("# List of booked rooms #\n", h1.listBookedRooms());
+console.log("####################")
+h1.rooms.forEach((r) => console.log("# Room #\n", r, "\n", r.isBooked()? "": " not", "booked"));
+
